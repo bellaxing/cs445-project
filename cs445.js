fb176819546342c35a6d00a634afc71d60cf9ef0
reddaGz <44gebre@gmail.com>
2021-06-05 14:41:31 -0500

    cs445 js file updated

--- cs445.js
+++ cs445.js
@@ -22,19 +22,17 @@ function pageOnload() {
           let template = `     
             <div class="col">
                 <h3> user information:</h3>
-                <p>id: ${id}</p>
                 <p>name: ${data.name}</p>
                 <p>Email:${data.email} </p>
                 <p style="color: red;font-size: larger;">Address</p>
                 <p>Street:${data.address.street} </p>
-                <p>City:${data.address.city} </p>
                 <p>City:${data.address.city} </p>
                 <p>Zip:${data.address.zipcode} </p>
                 <button id="idBut" value="${id} " style="background-color: aqua;">Get posts</button>
             </div>     
         `;
           const div = document.createElement("div");
-          div.classList = "row border-top";
+          //div.classList = "row border-top";
           div.innerHTML = template;
           userList.append(div);
           let getPost = document.getElementById("idBut");
