1775cac45680acda75d58099b4e2b85fbec79676
reddaGz <44gebre@gmail.com>
2021-06-05 22:22:30 -0500

    added current location method

--- cs445.js
+++ cs445.js
@@ -18,6 +18,32 @@ function pageOnload() {
         user
           .pipe(filter((element) => element.id === userId))
           .subscribe((data) => {
+            let lati=data.address.geo.lat;
+            let long=data.address.geo.lng
+            let currentLocation;
+            console.log(data.address.geo.lat)
+            console.log(data.address.geo.lng)
+            fetchCurrentLocation()
+            async function fetchCurrentLocation(){
+              let resultLocations= await fetch('https://mapquestapi.com/geocoding/v1/reverse?key=q5N7YWFQnHlQCfx0KyD5d1qoATAAFezV&location='+lati+','+long)
+              let jsonLocation=await resultLocations.json();
+              //console.log(jsonLocation)
+              currentLocation=jsonLocation.results[0].locations[0].street;
+             if(currentLocation===""){
+              //console.log("127 Marthin Luther king blvd")
+               navigator.geolocation.getCurrentPosition(latLong,failToLoad)
+            }
+             function latLong(position){
+                lati=position.coords.latitude;
+                long=position.coords.longitude;
+                fetchCurrentLocation();
+            }
+            function failToLoad(){
+              alert("current locations failed to load")
+            }
+            console.log(currentLocation);
+            }
+            
             let id = data.id;
             let userTemplate = `     
               <div class="col">
@@ -28,6 +54,7 @@ function pageOnload() {
                   <p>Street:${data.address.street} </p>
                   <p>City:${data.address.city} </p>
                   <p>Zip:${data.address.zipcode} </p>
+                  <p>Current location:${currentLocation} </p>
                   <button id="idBut" value="${id} " style="background-color: aqua;">Get posts</button>
               </div>     
           `;
@@ -98,6 +125,5 @@ function pageOnload() {
       }
     }
   }
-  
 }
 window.onload = pageOnload;
