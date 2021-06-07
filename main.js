b33147b7a0c057981ec15fc5f3d1174c073372a6
Ashenafi Gebretekle <ashenafiaynalemg@gmail.com>
2021-06-07 03:14:25 -0500

    fetching json file code added

---
+++ main.js
@@ -0,0 +1,29 @@
+"use strict";
+
+window.onload = function () {
+  const { from } = rxjs;
+  const { filter } = rxjs.operators;
+
+  document.getElementById("SID").onclick = displayUser;;
+
+
+
+
+  function displayUser() {
+    const userId = parseFloat((document.getElementById("UID").value));
+    let userList = document.getElementById("UID");
+    fetchUser();
+
+    ///====
+    async function fetchUser() {
+      userList.innerHTML = "";
+      let result = await fetch("https://jsonplaceholder.typicode.com/users");
+      let userFetch = await result.json();
+      const user = from(userFetch);
+      user
+        .pipe(filter((element) => element.id === userId))
+        .subscribe((data) => {
+          displayUserData(data);
+        });
+    }
+    //=====\
