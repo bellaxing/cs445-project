0c9bf910969e3da7db12d6b3147e1a8f0236b761
gakalu <69823298+gakalu@users.noreply.github.com>
2021-06-07 10:18:13 -0500

    .

--- myPro.js
+++ myPro.js
@@ -1,1 +1,19 @@
-
+function pageOnload() {
+    //http://jsonplaceholder.typicode.com/users
+    const search = document.getElementById("search");
+    const { from } = rxjs;
+    const { filter } = rxjs.operators;
+    search.onclick = displayUserinformation;
+    function displayUserinformation() {
+      User();
+      const userId = Number(document.getElementById("id").value);
+      async function User() {
+        let userList = document.getElementById("users");
+        userList.innerHTML = "";
+        let result = await fetch("http://jsonplaceholder.typicode.com/users");
+        let userFetch = await result.json();
+        console.log(userFetch);
+            };
+      }
+    }
+  window.onload = pageOnload;
