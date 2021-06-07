d1988efa1aad59631cd32bde16644ec7b617637e
AnuPoudyal <79480470+AnuPoudyal@users.noreply.github.com>
2021-06-07 13:52:01 -0500

    created user details with name,address,email

--- user.js
+++ user.js
@@ -1,10 +1,17 @@
-const getUser=async()=>{
-    const response=await fetch("http://jsonplaceholder.typicode.com/users");
-    const data=await response.json();
-    console.log(data);
-}
-getUser();
 
-
-const input=`<input type="text" />`
-document.getElementById("root").append(input);
+(async () => {
+    // Get User Details
+    const response = await fetch('http://jsonplaceholder.typicode.com/users');
+    const data = await response.json();
+    console.log('User Details---->', data);
+  
+    data.forEach((user, index) => {
+      document.getElementById('user').innerHTML += `<label>Name:</label>
+      <input type = "text"  id=${index}name value=${user.name} />
+      <label>Email:</label>
+      <input type = "text"  id=${index}email value=${user.email} />
+      <label>Address:</label>
+      <input type = "text" id=${index}address value=${user.address.city} />
+      <br/>`;
+    });
+})();
