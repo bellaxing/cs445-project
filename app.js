d6c8d0c7f801d5edbb6d87e4934ba7a9ba277267
Eyasu Gebrehiwot <jgebrehiwot@gmail.com>
2021-06-07 18:26:29 -0500

    Feature modified

--- app.js
+++ app.js
@@ -1,45 +1,26 @@
 
 
-// Information to reach API
-const geolocatorapiKey = '';
-const url1 = '';
-
-const url = "https://jsonplaceholder.typicode.com/posts/1";
 
 // Page elements 
+// Starting with defining the page elements 
 const inputfield = document.querySelector("#input");
-const commentsButton = document.querySelector("comments");
-const responsefield = document.querySelector("#responsefield");
+const submitButton = document.querySelector("#submitbtn");
+const responsefield = document.getElementById("responsefield");
+const commentButton = document.querySelector("#commentbtn")
 
-// AJAX functions
-const identity = () => {
-    const userId = inputfield.value;
-    const data = JSON.stringify({ destination: userId });
-    const xhr = new XMLHttpRequest();
-    const xhr.responseType = 'json';
-    if (xhr.readyState == XMLHttpRequest.DONE) {
-        renderRawResponse(xhr.response);
+// Information to reach API
+
+
+function request() {
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+        .then(function (response) {
+            return response.json();
+        })
+        .then((response) => {
+            console.log(response)
+            responsefield.innerHTML = "Hello";
+                //`User Id = ${response.userId}`
+    })
 }
-
-}
-
-window.onload = function () {
-    const url = 'http://www.mapquestapi.com/geocoding/v1/reverse?key=tWbaBBw92lD6YInMlGnx6xwwj8GpGZVO&location=30.333472,-81.470448';
-    fetch(url).then(response => response.json())
-        .then(console.log);
-}
-
-// const data = JSON.stringify({ id: '200' });
-// xhr.responseType = 'json';
-// xhr.onreadystatechange = () => {
-//     if (xhr.readyState === XMLHttpRequest.DONE) {
-//         return xhr.response;
-//     }
-// }
-
-xhr.open("POST", url)
-xhr.send(data)
-
-fetch('https://jsonplaceholder.typicode.com/posts/1')
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+submitButton.addEventListener('click', request);
+// 
---
+++ helperFunctions.js
@@ -0,0 +1,28 @@
+//let val;
+// function updatevalue(el) {
+//     const queryParams = '?sl='
+//     const idQuery = inputfield.value;
+//     const endpoint = `${url}${queryParams}${idQuery}`;
+//     val = el.target.value;
+//     console.log(val)
+//     fetch(endpoint)
+//         .then((response) => response.json())
+//         .then((json) => console.log(json));
+// }
+// inputfield.addEventListener('change', updatevalue);
+// // used for Geolocation
+// window.onload = function () {
+//     const url = 'http://www.mapquestapi.com/geocoding/v1/reverse?key=mQG1RGTPfqewDlLpKPG4A0OytOCkvVsG&location=30.333472,-81.470448';
+//     fetch(url).then(response => response.json())
+//         .then(console.log);
+// }
+//     // Clears previous results and display results to webpage
+// const displaySuggestions = (event) => {
+//     event.preventDefault();
+//     while (responseField.firstChild) {
+//         responseField.removeChild(responseField.firstChild);
+//     }
+//     getSuggestions();
+// };
+
+// commentsButton.addEventListener('click', displaySuggestions);
