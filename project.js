e19b311226c61649816eae8be1790af5f05866a4
Hailemariam Gebrekidan <midextam@gmail.com>
2021-06-11 08:18:11 -0500

    project.js

--- project.js
+++ project.js
@@ -4,26 +4,26 @@ window.onload = function () {
   
     document.getElementById("search-user").onclick = showUser;
     function showUser() {
-      const userId = parseFloat((document.getElementById("user-id").value));
+      const userId = (document.getElementById("user-id").value);
       let userList = document.getElementById("user-list");
       fetchUser();
     
       async function fetchUser() {
         userList.innerHTML = "";
         let response = await fetch("https://jsonplaceholder.typicode.com/users");
-        let userFetch = await response.json();
-        const user = from(userFetch);
+        let json = await response.json();
+        const user = from(json);
         user
-          .pipe(filter((element) => element.id === userId))
-          .subscribe((data) => {
-            showUserData(data);
+          .pipe(filter((user) => user.id == userId))
+          .subscribe((user) => {
+            showUserData(user);
           });
       }
   
       function showUserData(data) {
         let lati = data.address.geo.lat;
         let long = data.address.geo.lng;
-        let location;
+        
   
         fetchLocation();
   
@@ -37,18 +37,18 @@ window.onload = function () {
           let location = jsonLocation.results[0].locations[0].street;      
           console.log(location);
         }
-        let id = data.id;
+        
         let template = `     
                     <div class="col">
-                        <h3>Employee Info:</h3>
-                        <p>name: ${data.name}</p>
-                        <p>Email:${data.email} </p>
-                        <p style="color:red;font-size: larger;"></p>
-                        <p>Street:${data.address.street} </p>
-                        <p>City:${data.address.city} </p>
-                        <p>Zip:${data.address.zipcode} </p>
+                        <h4>Employee Info:</h4>
+                        <p>Full Name: ${data.name}</p>
+                        <p>Email: ${data.email} </p>
+                        
+                        <p>Street: ${data.address.street} </p>
+                        <p>City: ${data.address.city} </p>
+                        <p>Zipcode: ${data.address.zipcode} </p>
                         <p>Latitude: ${data.address.geo.lat} Longitude: ${data.address.geo.lng}</p>
-                        <button id="idBut" value="${id} " style="background-color:white">Get User Post</button>
+                        <button id="idBut" value="${data.id} " style="background-color:white">Get User Post</button>
                     </div>     
                 `;
   
@@ -69,21 +69,21 @@ window.onload = function () {
           let postJson = await postResult.json();
   
           from(postJson)
-            .pipe(filter((elem) => elem.userId === +userId))
-            .subscribe((postData) => {
-              showUserPost(postData);
+            .pipe(filter((user) => user.userId == userId))
+            .subscribe((user) => {
+              showUserPost(user);
             });
         }
         function showUserPost(postData) {
           console.log(postData);
-          let postId = postData.id;
+        
           let userPostTemplate = `     
                     <div class="col">
                     <h5> User post</h5>
                         <p>Employee Id: ${postData.id}</p>
                         <p>Title: ${postData.title}</p>
-                        <p>Body:${postData.body} </p>
-                        <button id="commentBut" value="${postId} " style="background-color: white;">Get User Comment</button>
+                        <p>Body: ${postData.body} </p>
+                        <button id="commentBut" value="${postData.id} " style="background-color: white;">Get User Comment</button>
                         <div id="list-comments"> </div>
                     </div>     
                 `;
@@ -106,7 +106,7 @@ window.onload = function () {
             let comId = Number(postCommentBut.value);
   
             from(commentJson)
-              .pipe(filter((commentDta) => commentDta.postId === comId))
+              .pipe(filter((commentDta) => commentDta.postId == comId))
               .subscribe((commentDta) => {
                 showComments(commentDta);
               });
@@ -117,9 +117,9 @@ window.onload = function () {
               let commentTemplate = `     
                                 <div class="col">
                                 <h6 ">User Comment</h6>
-                                    <p>name:  ${commentDta.name}</p>
-                                    <p>email:   ${postData.body} </p>
-                                    <p>comment: ${postData.body} </p>
+                                    <p>Name:  ${commentDta.name}</p>
+                                    <p>Email:   ${postData.body} </p>
+                                    <p>Comment: ${postData.body} </p>
                                 </div>     
                             `;
   
@@ -134,69 +134,6 @@ window.onload = function () {
   }
 
   //==============================================================================
-
-
-
-
-
-
-
-
-
-//     async function displayPostHtml(){
-//         const employeeDiv=document.getElementById("post");
-//         employeeDiv.innerHTML="";
-//         const id=document.getElementById("inputId").value;
-//         const users=await postSingleUser();
-//         users.filter(user=>user.userId==id)
-//              .forEach(user=>{
-//                let template=  `
-//              <div class="col">
-//                     <p>Name ${user.userId} </p>
-//                     <p>Id Number: ${user.id}</p>
-//                     <p>Title: ${user.title}</p>
-//                     <p>Bosy: ${user.body}</p>
-                    
-//                 </div> 
-//              `
-//               const div= document.createElement("div");
-//               div.innerHTML = template;
-//               employeeDiv.append(div);
-//              })
-//     } async function commentSingleUser(){
-//         const responseBody=await fetch('https://jsonplaceholder.typicode.com/comments');
-//         const json=await 
-//     }
-
-// window.onload = async function() {
-
-//     document.getElementById("getBtn").onclick = async function() {
-
-//         await displayUserInHtml();
-//         document.getElementById("getPost").onclick=async function(){
-//             await displayPostHtml();
-//         }
-//     }
-    
-
-// }
-
-// async function fetchSingleUser() {
-//     let responseBody = await fetch('http://jsonplaceholder.typicode.com/comments'); //'https://randomuser.me/api/'
-//     let json = await responseBody.json();
-//     return json.results[0];
-// }
-
-// async function fetchUsers(len) {
-//     const users = [];
-//     for (let i = 0; i < len; i++) {
-//         users[i] = await fetchSingleUser();
-//     }
-//     return users;
-// }
-
-
-
 /*
 window.onload = function() {
      
