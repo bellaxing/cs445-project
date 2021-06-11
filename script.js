f2323a6b6200ce5224e21ec8ef2b182e28d81e75
danielethi <60756630+danielethi@users.noreply.github.com>
2021-06-11 00:45:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
 
-//map
-   function map(){
-    const url = 'http://www.mapquestapi.com/geocoding/v1/reverse?key=tWbaBBw92lD6YInMlGnx6xwwj8GpGZVO&location=30.333472,-81.470448';
-    fetch(url).then(response => response.json())
-        .then(console.log);
-}
-   // will fetch every obj with userID=1
-   let use=fetch('http://jsonplaceholder.typicode.com/posts?userId=1')
-   .then(r=>r.json())
-   .then(d.console.log(d));
-
-
-// gives postId?=1
-   let ret=fetch('http://jsonplaceholder.typicode.com/comments?postId=1') 
-   .then(res=>res.json())
-   .then(out=>console.log(out))
