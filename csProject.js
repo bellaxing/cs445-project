febb32e0250ac5b5780a0c7edd17ed459bff2ba1
mft123eli <89947358+mft123eli@users.noreply.github.com>
2021-11-17 15:59:16 -0600

    added some css style

--- csProject.js
+++ csProject.js
@@ -12,26 +12,28 @@ async function userIdSearch() {
 
   const userInformation = document.getElementById("userInfo");
   userInformation.innerHTML = "";
-  let div1 = document.createElement("div");
-  div1.classList = "col-6";
-  userInformation.append(div1);
+  // let div1 = document.createElement("div");
+  // div1.classList = "col-6";
+  // userInformation.append(div1);
   let template = `
            
-                <div> <h3> User Information </h1></div>
+                <div> <h3> User Information </h3></div>
                 
-                 <div> Name: ${userInfo.name}</div>
+                 <div> Name: ${userInfo.name}</div><br>
                  <div> Email: ${userInfo.email}</div>
                  <div> <h5>Addrees <h5></div>
-                 <div> Street: ${userInfo.address.street}</div>
-                 <div> City: ${userInfo.address.city}</div>
-                 <div> Zip: ${userInfo.address.zipcode}</div>
-                 <div> Zip: ${userInfo.address.currentLocation}</div>
+                 <div> Street: ${userInfo.address.street}</div><br>
+                 <div> City: ${userInfo.address.city}</div><br>
+                 <div> Zip: ${userInfo.address.zipcode}</div><br>
+                 <div> Zip: ${userInfo.address.currentLocation}</div> <br>
                
-           
             <hr>
             `;
+
   let getPost = document.createElement("button");
   getPost.innerHTML = "Get Posts";
+  getPost.style.backgroundColor = "blue";
+
   getPost.onclick = async function () {
     document.getElementById("posts").innerHTML = "";
     const IdNumber = document.getElementById("idNo").value;
@@ -43,23 +45,24 @@ async function userIdSearch() {
     postInfo.forEach((post) => {
       let postContent = document.createElement("div");
 
-      postContent.innerHTML = `<b>Posts:</b><br> title: ${post.title} <br> body:${post.body} <br>`;
+      postContent.innerHTML = `<b> User posts:</b><br> title: ${post.title} <br><br> body:${post.body} <br><br>`;
 
       let viewComment = document.createElement("button");
       viewComment.style.backgroundColor = "blue";
       let commentInfo = document.createElement("div");
       viewComment.innerHTML = "View Comments";
+
       viewComment.onclick = async function () {
         commentInfo.innerHTML = "";
         let result = await fetch(
           "https://jsonplaceholder.typicode.com/comments?postId=" + post.id
         );
         let comments = await result.json();
-        console.log(comments);
+
         comments.forEach((comment) => {
           let commentContent = document.createElement("div");
 
-          commentContent.innerHTML = `<p>Comment:</p>name: ${comment.name}<br> email: ${comment.email} <br> body: ${comment.body} <br>`;
+          commentContent.innerHTML = `<p style="color:Tomato "> Comment:</p> name: ${comment.name}<br> email: ${comment.email} <br> body: ${comment.body} <br>`;
           commentInfo.append(commentContent);
         });
       };
