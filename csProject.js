75616b1c510c81d0d8a42fa32edcddca3da8a9d6
mft123eli <89947358+mft123eli@users.noreply.github.com>
2021-11-17 12:04:37 -0600

    creat post information

--- csProject.js
+++ csProject.js
@@ -12,25 +12,54 @@ async function userIdSearch() {
 
   const userInformation = document.getElementById("userInfo");
   userInformation.innerHTML = "";
+  let div1 = document.createElement("div");
+  div1.classList = "col-6";
+  userInformation.append(div1);
   let template = `
-            <div class="col-6" >
-                <div> <h1> User Information </h1></div>
+           
+                <div> <h3> User Information </h1></div>
                 
                  <div> Name: ${userInfo.name}</div>
                  <div> Email: ${userInfo.email}</div>
-                 <div> <h3>Addrees <h3></div>
+                 <div> <h5>Addrees <h5></div>
                  <div> Street: ${userInfo.address.street}</div>
                  <div> City: ${userInfo.address.city}</div>
                  <div> Zip: ${userInfo.address.zipcode}</div>
-
-            </div>
-            <div class="col-6">
-
-            </div>
+               
+           
             <hr>
             `;
-  let div = document.createElement("div");
-  div.classList = "row";
-  div.innerHTML = template;
-  userInformation.appendChild(div);
+  let getPost = document.createElement("button");
+  getPost.innerHTML = "Get Posts";
+  getPost.onclick = async function () {
+    const IdNumber = document.getElementById("idNo").value;
+
+    const postsUser = await fetch(
+      "https://jsonplaceholder.typicode.com/posts?userId=" + IdNumber
+    );
+    const postInfo = await postsUser.json();
+    postInfo.forEach((post) => {
+      document.getElementById(
+        "posts"
+      ).innerHTML += `<b>Posts:</b><br> title: ${post.title} <br> body:${post.body} <br>`;
+      let viewComment = document.createElement("button");
+      let commentBox = document.createElement("div");
+      viewComment.innerHTML = "View Comments";
+      viewComment.onclick = async function () {
+        let result = await fetch(
+          "https://jsonplaceholder.typicode.com/comments?postId=" + post.id
+        );
+        let comments = await result.json();
+        comments.forEach((comment) => {
+          commentBox.innerHTML = `name: ${comment.name} email: ${comment.email} body: ${comment.body}`;
+        });
+      };
+      document.getElementById("posts").append(viewComment);
+      document.getElementById("posts").append(commentBox);
+    });
+  };
+  document.getElementById("userInfo").innerHTML = template;
+  document.getElementById("userInfo").append(getPost);
+
+  //
 }
