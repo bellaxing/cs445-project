df69d7252441649513daea7ed20ba024fa1716c6
winny-coder <81041180+winny-coder@users.noreply.github.com>
2021-11-17 17:05:34 -0600

    fetching data

--- pro.js
+++ pro.js
@@ -3,3 +3,13 @@ const { filter } = rxjs.operators;
 window.onload = function () {
    
     document.getElementById('searchbtn').addEventListener('click', userFetch)
+}
+async function userFetch() {
+
+    let result = await fetch('https://jsonplaceholder.typicode.com/users')
+    let res = await result.json()
+    document.getElementById('detailcol').innerHTML = ""
+    document.getElementById('commentcol').innerHTML = ""
+
+    postEmployees(res)
+}
