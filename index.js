88080b19e987f19841616d29afd82a146c6373c5
meroneyeram <merrhaile@gmail.com>
2021-11-17 00:45:15 -0600

    Added async function fetchPosts

--- index.js
+++ index.js
@@ -6,10 +6,9 @@ window.onload = function () {
 }
 
 async function fetchUsers() {
-    const response = await fetch('https://jsonplaceholder.typicode.com/users/' + 
-    document.getElementById("user-id").value)
+    const response = await fetch('https://jsonplaceholder.typicode.com/users/' + document.getElementById("user-id").value)
     const user = await response.json()
-    console.log(user)
+    
     const divUserInfo = document.querySelector(".col-4");
     divUserInfo.innerHTML = "<pre> </pre>" 
         + "<p> <b>Name</b>: " + user.name + "</p>" 
@@ -20,9 +19,26 @@ async function fetchUsers() {
         + "<p> <b>Zip</b>: " + user.address.zipcode + "</p>"
         + "<p> <b>Current location</b>: " + user.address.zipcode + "</p>"
         + "<button class='btn btn-secondary' id='post-btn'> Get post</button>";
+        
+        
+
+}
+
+ async function fetchPosts () {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts?userId=' + document.getElementById("user-id").value)
+    const userPost = await response.json()
     
-    // const postBtn = document.createElement('button')
-    // postBtn.innerHTML = "Get post";
-    // postBtn.id = "post-btn"
-    // divUserInfo.appendChild(postBtn)
+    userPost.forEach(function(post){
+        const divUserInfo = document.querySelector(".col-7.row");
+        const divPost= document.createElement("div")
+        divPost.classList = "col-12 postId-" + document.getElementById("user-id").value
+        divPost.innerHTML = 
+            "<hr>"
+            + " <h5>User Post </h5>"
+            + "<p><b>Title</b>:" + post.title +"</p> "
+            + "<p><b>Body</b>:" + post.body + "</p>"
+            + "<button class='btn btn-secondary comment-btn' type='button'> Comment </button>"
+        
+        divUserInfo.appendChild(divPost)
+    });
 }
