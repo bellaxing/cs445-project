8da72fdb77bf876c8a461d712399988bf17a1c80
winny-coder <81041180+winny-coder@users.noreply.github.com>
2021-11-17 17:10:13 -0600

    created users

--- pro.js
+++ pro.js
@@ -13,3 +13,30 @@ async function userFetch() {
 
     postEmployees(res)
 }
+function postEmployees(users) {
+    let inputId = +document.getElementById('userinput').value;
+   
+    from(users).pipe(
+        filter(x => x.id == inputId)
+    ).subscribe(data => {
+       
+        let innerdiv = document.getElementById('uInfo');
+        innerdiv.innerHTML = "";
+
+        let user = `
+           <div style="text-align:center"class="col">
+           <p class="text-end"><b>Name: ${data.name}</b></p>
+           <p class="text-end">Email: ${data.email}</p>
+           <p class="text-end">Address: ${data.address.city +" "+ data.address.street +" "+ data.address.zipcode}</p>
+           <p class="text-end">Current Location: ${data.address.geo.lng + ' '+data.address.geo.lat}</p>
+           </div>
+           
+           `;
+       
+        let row = document.createElement('div');
+        row.className = 'row';
+        row.innerHTML = user;
+        innerdiv.appendChild(row);
+
+    })
+}
