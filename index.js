a3edf4548d9bc3cafb92d44ab8c17d6a68d27be9
Maicobe20 <mikeberhane2020@gmail.com>
2021-11-17 04:52:22 -0600

    done showing comments

--- index.js
+++ index.js
@@ -63,9 +63,39 @@ function showPosts(p){
        let dBtn = document.createElement('button')
        row.appendChild(dBtn)
        row.appendChild(hr)
-    //    dBtn.onclick = createDetail
+       dBtn.onclick = createDetail
        dBtn.className = "btn btn-success";
        dBtn.innerHTML = "Comments";
        innerdiv.appendChild(row);
+    })
+}
+function createDetail(){
+    console.log("Mike")
+    let inputId = +document.getElementById('userinput').value;
+    // let importPost = fetch('https://jsonplaceholder.typicode.com/posts?userId='+inputId+'/comments')
+    //         importPost.then(x=>x.json()).then(p=>showPosts(p))
+    let innerdiv = document.getElementById('commentcol');
+    innerdiv.innerHTML=""
+  
+    from(fetch('https://jsonplaceholder.typicode.com/comments?postId='+inputId).then(x=>x.json()))
+    .subscribe(data=>{
+     console.log(data)
+     data.forEach(com=>{
+       
+         let post = `
+            <div class="col">
+            <p class="text-end"><b>Name: ${com.name}</b></p>
+            <p class="text-end"><b>email: ${com.email}</b></p>
+            <p class="text-end">Comment: ${com.body}</p>
+            </div>
+            
+            `;
+         let row = document.createElement('div');
+         let hr = document.createElement('hr')
+            row.className = 'row';
+            row.innerHTML = post;
+            row.appendChild(hr)
+            innerdiv.appendChild(row);
+     })
     })
 }
