189a58c156706a0674137a45e67e371cfa58c47a
Barni_Classic <76923911+barniclassic@users.noreply.github.com>
2021-11-17 18:28:26 -0600

    func3

--- import.js
+++ import.js
@@ -56,6 +56,56 @@ async function usersList(input) {
     document.getElementById("btn-post").addEventListener("click", function(){postList(input)});
 }
 
+async function postList(input) {
+  let promise = await fetch("http://jsonplaceholder.typicode.com/posts?userId=" + input);
+  let posts = await promise.json();
+  let row = document.getElementById("posts");
+  row.innerHTML = "";
+    from(posts)
+      .subscribe((emp) => {
+        let title = document.createElement("div");
+        title.innerHTML = emp.title;
+        let post = document.createElement("div");
+        post.innerHTML = emp.body;
+        let hr = document.createElement("hr");
+        let comment = document.createElement("input");
+        comment.type = "button";
+        comment.classList = "btn btn-info w-30";
+        comment.id = "btn-comment";
+        comment.value = "Comments";
+        row.appendChild(title);
+        row.appendChild(post);
+        row.appendChild(comment);
+        row.appendChild(hr);
+      });
+      document.getElementById("btn-comment").addEventListener("click", function(){commentList(input)});
+};
+
+async function commentList(input) {
+    let promise = await fetch("http://jsonplaceholder.typicode.com/comments?postId=" + input);
+    let posts = await promise.json();
+    console.log(posts);
+    let row = document.getElementById("comments");
+    // row.innerHTML = "";
+      from(posts)
+        .subscribe((emp) => {
+            console.log(emp);
+            let name = document.createElement("span");
+            name.classList = "text-right";
+            name.innerHTML = emp.name;
+            let email = document.createElement("div");
+            email.classList = "text-right";
+            email.innerHTML = emp.email;
+            let comment = document.createElement("div");
+            comment.innerHTML = emp.body;
+            let hr = document.createElement("hr");
+            row.appendChild(name);
+            row.appendChild(email);
+            row.appendChild(comment);
+            row.appendChild(hr);
+            console.log(row);
+        });
+  };
 
 
  
