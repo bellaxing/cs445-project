aed49a1f0ad7fd3737bb2a9e4851443587dcab38
yoelng <yoelgoitom25@gmail.com>
2021-11-17 18:58:17 -0600

    style added

--- index.js
+++ index.js
@@ -1,41 +1,65 @@
+const { from } = rxjs;
+const { filter } = rxjs.operators;
 window.onload = function () {
-  const { from } = rxjs;
-  const { filter } = rxjs.operators;
-  let input = document.getElementById("inp");
   let btn = document.getElementById("btn");
   btn.onclick = data;
+};
 
-  function data() {
-    fetch("http://jsonplaceholder.typicode.com/users")
-      .then((data) => data.json())
-      .then((data) => doSome(data));
-  }
-  function doSome(arr) {
-    let card = "";
-    userId = arr.filter((item) => item.id == input.value);
-    userId.forEach((item) => {
-      card = `
-        <p>${item.name}</p>
-        <p>${item.email}</p>
-        <p>${item.address.street} ${item.address.suite} ${item.address.city} ${item.address.zipcode}</p>
-        <p>${item.address.geo.lat} ${item.address.geo.lng}</p>
-        <br>
-        `;
+async function data() {
+  let result = await fetch("https://jsonplaceholder.typicode.com/users");
+  let res = await result.json();
+  document.getElementById("first").innerHTML = "";
+  document.getElementById("second").innerHTML = "";
+
+  doSome(res);
+}
+
+function doSome(users) {
+  let inputId = +document.getElementById("inp").value;
+
+  from(users)
+    .pipe(filter((x) => x.id == inputId))
+    .subscribe((data) => {
+      let innerdiv = document.getElementById("user");
+      innerdiv.innerHTML = "";
+
+      let user = `
+         <div style="text-align:center"class="col">
+         <p class="text-center"><b>Name: ${data.name}</b></p>
+         <p class="text-center"> <b>Email: ${data.email}</b></p>
+         <p class="text-center"> <b>Address: ${
+           data.address.city +
+           " " +
+           data.address.street +
+           " " +
+           data.address.zipcode
+         }</b></p>
+         <p class="text-center"><b>Current Location: ${
+           data.address.geo.lng + " " + data.address.geo.lat
+         }</b></p>
+         </div>
+         
+         `;
+      let importPost = fetch(
+        "https://jsonplaceholder.typicode.com/posts?userId=" + inputId
+      );
+      importPost.then((x) => x.json()).then((p) => showPost(p));
+
+      let row = document.createElement("div");
+      row.className = "row";
+      row.innerHTML = user;
+      innerdiv.appendChild(row);
     });
-    let posts = fetch(
-      "https://jsonplaceholder.typicode.com/posts?userId=" + input.value
-    );
-    posts.then((x) => x.json()).then((data) => showPost(data));
-    document.getElementById("user").innerHTML = card;
-  }
+}
 
-  function showPost(p) {
-    let innerdiv = document.getElementById("first");
-    let phead = document.createElement("h4");
-    phead.innerHTML = "POSTS";
-    innerdiv.appendChild(phead);
-    from(p).subscribe((data) => {
-      let post = `
+function showPost(p) {
+  let innerdiv = document.getElementById("first");
+  let phead = document.createElement("h4");
+  phead.innerHTML = "POSTS";
+  phead.className = "offset-5 mb-5";
+  innerdiv.appendChild(phead);
+  from(p).subscribe((data) => {
+    let post = `
        <div class="col">
 
        <p class="text-end"><b> ${data.title}</b></p>
@@ -43,32 +67,34 @@ window.onload = function () {
        </div>
 
        `;
-      let row = document.createElement("div");
-      let hr = document.createElement("hr");
-      row.className = "row";
-      row.innerHTML = post;
-      let dBtn = document.createElement("button");
-      row.appendChild(dBtn);
-      row.appendChild(hr);
-      dBtn.onclick = comments;
-      dBtn.className = "btn btn-secondary";
-      dBtn.innerHTML = "Show Comment";
-      innerdiv.appendChild(row);
-    });
-  }
-  function comments() {
-    let innerdiv = document.getElementById("second");
-    innerdiv.innerHTML = "";
-    let chead = document.createElement("h4");
-    chead.innerHTML = "Comments";
-    innerdiv.appendChild(chead);
-    from(
-      fetch(
-        "https://jsonplaceholder.typicode.com/comments?postId=" + input.value
-      ).then((x) => x.json())
-    ).subscribe((data) => {
-      data.forEach((com) => {
-        let post = `
+    let row = document.createElement("div");
+    let hr = document.createElement("hr");
+    row.className = "row";
+    row.innerHTML = post;
+    let dBtn = document.createElement("button");
+    row.appendChild(dBtn);
+    row.appendChild(hr);
+    dBtn.onclick = comments;
+    dBtn.className = "btn btn-success";
+    dBtn.innerHTML = "Show Comment";
+    innerdiv.appendChild(row);
+  });
+}
+function comments() {
+  let input = document.getElementById("inp");
+  let innerdiv = document.getElementById("second");
+  innerdiv.innerHTML = "";
+  let chead = document.createElement("h4");
+  chead.innerHTML = "Comments";
+  chead.className = "offset-5 mb-5";
+  innerdiv.appendChild(chead);
+  from(
+    fetch(
+      "https://jsonplaceholder.typicode.com/comments?postId=" + input.value
+    ).then((x) => x.json())
+  ).subscribe((data) => {
+    data.forEach((com) => {
+      let post = `
             <div class="col">
             <p class="text-end"><b>Name: ${com.name}</b></p>
             <p class="text-end"><b>email: ${com.email}</b></p>
@@ -76,13 +102,12 @@ window.onload = function () {
             </div>
 
             `;
-        let row = document.createElement("div");
-        let hr = document.createElement("hr");
-        row.className = "row";
-        row.innerHTML = post;
-        row.appendChild(hr);
-        innerdiv.appendChild(row);
-      });
+      let row = document.createElement("div");
+      let hr = document.createElement("hr");
+      row.className = "row";
+      row.innerHTML = post;
+      row.appendChild(hr);
+      innerdiv.appendChild(row);
     });
-  }
-};
+  });
+}
